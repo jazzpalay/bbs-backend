db2e6420feedf14df37597af7f366f44d6a28f45
RyougaMori <ryouga.mori.kv@gmail.com>
2026-01-20 23:36:35 +0900

    [update] jwt作成メソッドをサービスに実装

--- src/main/java/com/ryoga/bbs/domain/authentication/AuthenticationService.java
+++ src/main/java/com/ryoga/bbs/domain/authentication/AuthenticationService.java
@@ -1,6 +1,7 @@
 package com.ryoga.bbs.domain.authentication;
 
 import com.ryoga.bbs.domain.model.user.HashedPassword;
+import com.ryoga.bbs.util.JwtProvider;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +9,11 @@ import org.springframework.stereotype.Service;
 public class AuthenticationService {
 
     private final PasswordEncoder passwordEncoder;
+    private final JwtProvider jwtProvider;
 
-    public AuthenticationService(PasswordEncoder passwordEncoder) {
+    public AuthenticationService(PasswordEncoder passwordEncoder, JwtProvider jwtProvider) {
         this.passwordEncoder = passwordEncoder;
+        this.jwtProvider = jwtProvider;
     }
 
     public HashedPassword createHashPassword(String rowPassword) {
@@ -19,6 +22,10 @@ public class AuthenticationService {
 
     public boolean checkPassword(String rowPassword, String hashPassword) {
         return passwordEncoder.matches(rowPassword, hashPassword);
+    }
+
+    public String authenticate(String userId) {
+        return jwtProvider.createToken(userId);
     }
 
 }
--- src/main/java/com/ryoga/bbs/util/JwtProvider.java
+++ src/main/java/com/ryoga/bbs/util/JwtProvider.java
@@ -1,4 +1,36 @@
 package com.ryoga.bbs.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
 public class JwtProvider {
+    private final SecretKey key;
+    private final long expirationMs;
+
+    public JwtProvider(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expiration-ms}") long expirationMs
+    ) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        this.expirationMs = expirationMs;
+    }
+
+    public String createToken(String userId) {
+        Date now = new Date();
+        return Jwts.builder()
+                .setSubject(userId)   // 内部IDのみ
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + expirationMs))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .claim("role", "USER")
+                .compact();
+    }
 }

c56b645dcf44d8e6e725ed271ba6cb0064a69487
RyougaMori <ryouga.mori.kv@gmail.com>
2026-02-07 11:12:40 +0900

    [add] signOutAPIの作成

---
+++ src/main/java/com/ryoga/bbs/scenario/SignOutScenario.java
@@ -0,0 +1,23 @@
+package com.ryoga.bbs.scenario;
+
+import com.ryoga.bbs.domain.model.authentication.AuthenticationService;
+import com.ryoga.bbs.domain.model.authentication.RefreshToken;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SignOutScenario {
+    private final AuthenticationService authenticationService;
+
+    public SignOutScenario(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    public void signOut(String strRefreshToken){
+        if(strRefreshToken != null) {
+            //リフレッシュトークン検索
+            RefreshToken refreshToken = authenticationService.findByRefreshToken(strRefreshToken);
+            //リフレッシュトークン削除
+            authenticationService.deleteRefreshToken(refreshToken.getUserId(), refreshToken.getDeviceId());
+        }
+    }
+}

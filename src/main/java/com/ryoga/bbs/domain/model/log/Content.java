d3401801baede5715759378d647d8af9931ae7ac
jazzpalay <jazzmaster0428@gmail.com>
2026-04-25 22:27:15 +0900

    [update] ログ内容の文字制限を拡大

--- src/main/java/com/ryoga/bbs/domain/model/log/Content.java
+++ src/main/java/com/ryoga/bbs/domain/model/log/Content.java
@@ -5,8 +5,8 @@ import com.ryoga.bbs.domain.type.DomainLogicInvalidException;
 public class Content {
     private final String content;
     public Content(String content) {
-        if(content.length() > 200){
-            throw new DomainLogicInvalidException("ログ内容は200文字以内です");
+        if(content.length() > 20000){
+            throw new DomainLogicInvalidException("ログ内容は20000文字以内です");
         }
         this.content = content;
     }
